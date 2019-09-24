4d4003242af437f08632cd5dfd96ea29695b9cbf
Anna <townsendanna15@gmail.com>
2019-09-24 17:18:08 -0400

    did authentication and user test data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
     
-const server = require('./api/server');
+const server = require("./server");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`listening on port ${port}`));
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 
-const authenticate = require('./auth/auth-middleware');
-const authRouter = require('./auth/auth-router');
-const userRouter = require('./user/user-router');
+const authenticate = require("./");
+const authRouter = require("./api/auth/auth-router");
+const userRouter = require("./api/user/user-router");
 
 const server = express();
 
